2f60844fe0db3b2f1a70c9658483416758421c63
cachetdaspirine <leroyhugo@yahoo.fr>
2024-04-18 12:33:17 +0200

    remove scipy

--- fixed_density/mu5e_2.py
+++ fixed_density/mu5e_2.py
@@ -1,6 +1,6 @@
 
 import numpy as np
-from scipy.special import erfc
+#from scipy.special import erfc
 import sys
 sys.path.append('/home/hcleroy/PostDoc/aging_condensates/Simulation/Parallel_Simulation')
 sys.path.append('/home/hcleroy/Parallel_gillespie')
@@ -22,9 +22,6 @@ ell_tot = 10**3
 kdiff = 0.001
 Energy = -15
 
-avR = lambda L,N : 2*(np.exp(-1.5/(L/N)) * np.sqrt(L/N*6/np.pi)*(3+2*L/N) - 9*erfc(np.sqrt(3/2/(L/N))))/(9*L/N) #average distance between equilibrated nodes
-print(avR(ell_tot,Nlinker))
-print(np.sqrt(2*ell_tot/Nlinker/3))
 
 Nprocess = 10
 seeds = set()
@@ -34,9 +31,9 @@ seeds = list(seeds)
 args = [[ell_tot,Energy,kdiff,seeds[_],Nlinker,3] for _ in range(Nprocess)]
 
 # argument of the different classes
-cluster_arg = tuple([avR(ell_tot,Nlinker)]) # max distance
+cluster_arg = tuple([2.67]) # max distance
 MSD_arg = () # no argument 
-ISF_arg = (1/avR(ell_tot,Nlinker),10) # q_norm, q_num_sample
+ISF_arg = (0.374,10) # q_norm, q_num_sample
 NRG_arg = ()
 PCF_arg = (15,25) # max_distance,numb_bin
 PCF_L_arg = (ell_tot,30) # max_distance,numb_bin
